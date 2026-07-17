5276445e198aa7e40545702dc2c3a9e1a01ee5ee
ashique1213 <muhammedashique1213@gmail.com>
2026-07-17 14:40:27 +0530

    refactor(player): secure TMDB token, handle empty API responses, fix back navigation

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -12,31 +12,35 @@ const Player = () => {
     name:"",
     key: "",
     published_at: "",
-    typeof:""
+    type:""
   })
 
   const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ODg2MjFlMmE2MmU1NTk2ODNmNDY4YjliNDJkNWMxYyIsIm5iZiI6MTczNzAyNjUyMC45NDMsInN1YiI6IjY3ODhlYmQ4OTQ3YjE5Zjc4Yjk3ODIzZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qFUu1gi3dzcwojjsDdt6dj2-SL_4d4ms28sZQN5P9e0'
+      Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`
     }
   };
   
   useEffect(() => {
     fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
     .then(res => res.json())
-    .then(res => setapiData(res.results[0]))
+    .then(res => {
+      if (res.results && res.results.length > 0) {
+        setapiData(res.results[0]);
+      }
+    })
     .catch(err => console.error(err));
-  },[])
+  },[id])
   
   return (
     <div className='player'>
-      <img src={back_arrow} alt="" onClick={() => {navigate(-2) }}/>
+      <img src={back_arrow} alt="Go Back" onClick={() => {navigate(-1) }}/>
       <iframe width='90%' height='90%'
         src={`https://www.youtube.com/embed/${apiData.key}`} title='trailer' frameBorder='0' allowFullScreen></iframe>
       <div className="player-info">
-        <p>{apiData.published_at.slice(0,10)}</p>
+        <p>{apiData.published_at ? apiData.published_at.slice(0,10) : ""}</p>
         <p>{apiData.name }</p>
         <p>{ apiData.type}</p>
       </div>
